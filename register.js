bdac3c6ab15e003caf4c39ba81a21748dc5e2f7a
Teeraphong Vechatrakoolphaisan <101695122+Ithzz-Newton@users.noreply.github.com>
2022-04-21 16:32:45 +0700

    Add files via upload

---
+++ register.js
@@ -0,0 +1,70 @@
+var registerForm = document.getElementById("registerForm");
+
+var weight_kg = document.getElementById("weight_kg");
+var height_m = document.getElementById("height_m");
+var user_ages = document.getElementById("user_ages");
+var user_gender = document.getElementById("user_gender");
+
+
+
+
+
+var line_userId = document.getElementById("line_userId");
+var displayName = document.getElementById("displayName");
+
+var submitButton = document.getElementById("submitButton");
+var buttonSpinner = document.getElementById("buttonSpinner");
+var buttonText = document.getElementById("buttonText");
+var unknowError = document.getElementById("unknowError");
+
+
+function afterSubmit(e) {
+  e.preventDefault();
+
+  var info = {
+    student_name: student_name.value,
+    height_m: height_m.value,
+    user_ages: user_ages.value,
+    user_gender: user_gender.value,
+    
+    line_userId: line_userId.value,
+    displayName: displayName.value,
+    
+  };
+
+  var url =
+    "https://script.google.com/macros/s/AKfycbxt3S_B5TYo5r3jes2SFpHMx1RqrOA7vyXTAHONMcOgDLIz0pTfaqer2yFfLWgNnWk-3w/exec";
+
+  buttonText.textContent = "Saving..";
+  buttonSpinner.classList.remove("d-none");
+  submitButton.disabled = true;
+
+  fetch(url, {
+    method: "POST",
+    cache: "no-cache",
+    redirect: "follow",
+    body: JSON.stringify(info),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      window.location = "https://liff.line.me/1656999418-926ykYQX";
+      registerForm.reset();
+      buttonText.textContent = "Register";
+      buttonSpinner.classList.add("d-none");
+      submitButton.disabled = false;
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log("something Went Wrong");
+      unknowError.classList.remove("d-none");
+      setTimeout(function () {
+        unknowError.classList.add("d-none");
+        buttonText.textContent = "Register";
+        buttonSpinner.classList.add("d-none");
+        submitButton.disabled = false;
+      }, 1000);
+    });
+}
+
+registerForm.addEventListener("submit", afterSubmit);
